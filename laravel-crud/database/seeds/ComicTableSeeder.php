946b08e2435cb29cc26b88343fb9fe309c560b85
FabrizioRozzi <rozzi458@gmail.com>
2021-06-28 16:30:47 +0200

    problema

---
+++ laravel-crud/database/seeds/ComicTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+use App\Comic;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array_comic = config('comic');
+        //dump($array_comic);
+        foreach($array_comic as $comic){
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->description = $comic['description'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+            //dump($new_comic);
+            $new_comic->seve();
+        }
+    }
+}
---
+++ laravel-crud/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main');
+
+@section('content')
+    <h1>I nostri fumetti</h1>    
+@endsection
